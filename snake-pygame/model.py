7e5573d18ddb6e4244e70f12b382ab1975efdb1f
Parth <parthmshah1302@gmail.com>
2022-11-17 22:36:39 +0530

    feat: train done, implementing loss (rms)

--- snake-pygame/model.py
+++ snake-pygame/model.py
@@ -32,4 +32,37 @@ class QTrainer:
         self.criterion=nn.MSELoss()
 
     def train_step(self,state, action,reward, next_state,done):
-        pass
+        state=torch.tensor(state, dtype=torch.float)
+        action=torch.tensor(action,dtype=torch.long)
+        reward=torch.tensor(reward, dtype=torch.float)
+        next_state=torch.tensor(next_state, dtype=torch.float)
+        # if we already have multiple values, this is in the form (n,x)
+
+        if len(state.shape==1):
+            # if len is 1, state is x but we want (1,x) -> we want to append 1 dimension
+            state = torch.unsqueeze(state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            # converting done to a tuple
+            done=(done,)
+        
+        # Step 1: predicted Q values with current state
+        pred=self.model(state)
+        target=pred.clone()
+
+        # Step 2: Q_new = reward + gamma*max(next_predicted_QValue) -> only do this if not done
+        # pred.clone() - preds[argmax(action)] = Q_new
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new=reward[idx]+self.gamma*torch.max(self.model(next_state[idx]))
+            
+            target[idx][torch.argmax(action).item()]=Q_new
+
+        # zero_grad to empty the gradient (remember for PyTorch)
+        self.optimizer.zero_grad()
+
+        
+
+
